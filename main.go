bad7bf021fa036af3a5a22baeb7690620062aef4
Woo <toywoo02@naver.com>
2022-01-20 21:17:08 +0900

    14_array_enum_map

--- main.go
+++ main.go
@@ -1,34 +1,51 @@
 package main
 
-type Location struct {
-	x int
-	y int
-}
-
-type Attacker struct {
-	name string
-	hp   int
-	atk  int
-	def  int
-	loca Location
-}
-
-func NewAttacker(name string) *Attacker {
-
-	return &Attacker{name: name, hp: 100, atk: 10, def: 8, loca: Location{x: 10, y: 10}}
-}
-
-func (attacker *Attacker) getAttack(oppAttacker Attacker) {
-	attacker.hp -= oppAttacker.atk - attacker.def
-	println(oppAttacker.name, "attacks", attacker.name)
-}
+import "fmt"
 
 func main() {
-	attackLoc := Location{x: 10, y: 10}
-	attacker1 := Attacker{name: "attacker1", hp: 100, atk: 10, def: 8, loca: attackLoc}
-	attacker2 := Attacker{name: "attacker2", hp: 100, atk: 10, def: 8, loca: attackLoc}
-	attacker3 := NewAttacker("attacker3")
-	attacker1.getAttack(attacker2)
+	var arr = [5]int{1, 2, 3, 4, 5}
+	var sli = []int{1, 2, 3, 4, 5}
+	var num int
 
-	println(attacker1.hp, attacker2.hp, attacker3.def)
+	num = arr[0]
+	num = sli[0]
+
+	fmt.Println(num)
+
+	//
+
+	sli = append(sli, 6)
+	fmt.Println(sli)
+
+	//
+
+	for index, value := range arr {
+		fmt.Println("\n", index, value)
+	}
+
+	//
+
+	const (
+		KOREAN = iota
+		MATH
+		ENGLISH
+	)
+
+	var mulDiArr = [3][5]int{
+		{1, 2, 3, 4, 5},
+		{1, 2, 3, 4, 5},
+		{1, 2, 3, 4, 5},
+	}
+
+	fmt.Println(mulDiArr[KOREAN][2])
+
+	//
+
+	var gradeMap = make(map[int][]int, 3)
+
+	gradeMap[KOREAN] = []int{1, 2, 3, 4, 5}
+	gradeMap[MATH] = []int{1, 2, 3, 4, 5}
+	gradeMap[ENGLISH] = []int{1, 2, 3, 4, 5}
+
+	fmt.Println(gradeMap[KOREAN][2])
 }
