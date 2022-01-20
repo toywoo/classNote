31eca48d65ff8215e6d5445da2b63ba4772089fd
Woo <toywoo02@naver.com>
2022-01-20 20:29:21 +0900

    11_function

--- main.go
+++ main.go
@@ -2,38 +2,20 @@ package main
 
 import (
 	"fmt"
-	"unsafe"
 )
 
-func swap(ch1 *int, ch2 *int) {
-	var temp int
+func add(op1 int, op2 int) int {
+	return op1 + op2
+}
 
-	temp = *ch1
-	*ch1 = *ch2
-	*ch2 = temp
+func minus(op1 int, op2 int) int {
+	return op1 - op2
 }
 
 func main() {
-	var num int = 1
-	var pnum *int = &num
-
-	fmt.Println(num, *pnum)
-
-	fmt.Println(unsafe.Sizeof(pnum)) // 포인터의 크기는 8바이트이며 이론적으로 2^64까지 주소 접근이 가능합니다.
-
-	var str1 string = "Hello"
-	var pstr1 *string = &str1
-
-	fmt.Println(unsafe.Sizeof(pstr1))
-
-	//
-
 	var num1 int = 1
 	var num2 int = 2
+	var result int = add(num1, num2) + minus(num1, num2)
 
-	fmt.Println("Before: ", num1, num2)
-
-	swap(&num1, &num2)
-
-	fmt.Println("After: ", num1, num2)
+	fmt.Println(result)
 }
