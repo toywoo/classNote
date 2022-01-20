ba18807917994c05fb1fd30be7f6fe8f21714eb9
Woo <toywoo02@naver.com>
2022-01-20 17:49:33 +0900

    12_2_pointer_variable

--- main.go
+++ main.go
@@ -1,10 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
+
+func swap(ch1 *int, ch2 *int) {
+	var temp int
+
+	temp = *ch1
+	*ch1 = *ch2
+	*ch2 = temp
+}
 
 func main() {
-	var num1 int = 1
+	var num int = 1
+	var pnum *int = &num
 
-	fmt.Println(&num1)
-	fmt.Println(*&num1)
+	fmt.Println(num, *pnum)
+
+	fmt.Println(unsafe.Sizeof(pnum)) // 포인터의 크기는 8바이트이며 이론적으로 2^64까지 주소 접근이 가능합니다.
+
+	var str1 string = "Hello"
+	var pstr1 *string = &str1
+
+	fmt.Println(unsafe.Sizeof(pstr1))
+
+	//
+
+	var num1 int = 1
+	var num2 int = 2
+
+	fmt.Println("Before: ", num1, num2)
+
+	swap(&num1, &num2)
+
+	fmt.Println("After: ", num1, num2)
 }
