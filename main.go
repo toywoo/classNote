68c658cee821cf9fcef6bab1d3781b5a6c7d2125
toywoo <toywoo02@naver.com>
2022-02-15 22:56:11 +0900

    18_2_move_save

--- main.go
+++ main.go
@@ -3,20 +3,42 @@ package main
 import (
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
-func index(w http.ResponseWriter, req *http.Request) {
-	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+func index(res http.ResponseWriter, req *http.Request) {
+	res.Header().Add("Content-Type", "text/html; charset=utf-8")
+
 	htmlFile, readError := ioutil.ReadFile("./Client/note.html")
 	if readError != nil {
-		http.NotFound(w, req)
+		http.NotFound(res, req)
 	} else {
-		w.Write(htmlFile)
+		res.Write(htmlFile)
 	}
+}
+
+func save(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("save!"))
+}
+
+func handler(res http.ResponseWriter, req *http.Request) {
+	var path = req.URL.Path
+	splitedPath := strings.Split(path, "/")
+
+	switch splitedPath[1] {
+	case "":
+		index(res, req)
+	case "save":
+		save(res, req)
+
+	default:
+		http.NotFound(res, req)
+	}
+
 }
 
 func main() {
 	http.Handle("/Client/", http.StripPrefix("/Client", http.FileServer(http.Dir("./Client"))))
-	http.HandleFunc("/", index)
+	http.HandleFunc("/", handler)
 	http.ListenAndServe(":3002", nil)
 }
